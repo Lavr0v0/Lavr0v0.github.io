0d6616678c7274d7b80e1191e716f6de5b7a8fe5
Lav <tiphereth_lucis@outlook.com>
2026-03-20 14:29:05 -0700

    auto update

--- DnD/app.js
+++ DnD/app.js
@@ -166,7 +166,7 @@ const DnDPage = () => {
 
     h('style', { dangerouslySetInnerHTML: { __html: globalCSS } }),
 
-    h('div', { className: 'fixed inset-0 z-0 bg-cover bg-center grayscale contrast-[1.3] brightness-[0.25] pointer-events-none', style: { backgroundImage: 'url("../HomePageAssets/bg.webp")' } }),
+    h('div', { className: 'fixed inset-0 z-0 bg-cover bg-center grayscale contrast-[1.3] brightness-[0.4] pointer-events-none', style: { backgroundImage: 'url("../HomePageAssets/dnd-bg.webp")' } }),
     h('div', { className: 'fixed inset-0 z-[1] pointer-events-none opacity-[0.05] mix-blend-overlay', style: { backgroundImage: 'url("data:image/svg+xml,%3Csvg viewBox=\'0 0 256 256\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cfilter id=\'n\'%3E%3CfeTurbulence type=\'fractalNoise\' baseFrequency=\'0.9\' numOctaves=\'4\' stitchTiles=\'stitch\'/%3E%3C/filter%3E%3Crect width=\'100%25\' height=\'100%25\' filter=\'url(%23n)\'/%3E%3C/svg%3E")' } }),
 
     h('main', { className: 'relative z-10 w-full' },
